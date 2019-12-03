63bf67d047c750351d6d237c0267df35bf297001
TheGreekCuban <theodorealexander92@gmaill.com>
2019-12-03 14:40:15 -0500

    added heroku

---
+++ server.js
@@ -0,0 +1,38 @@
+//Importing Modules
+const mongoose = require('mongoose');
+const express = require('express');
+const path = require('path');
+
+//Importing routes
+const routes = require('./routes');
+
+//Define Global Variables
+const app = express();
+const log = consol.log;
+const PORT = process.env.PORT || 8080
+
+//Step 1
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/agavepv', {
+    useNewURLParser: true
+})
+
+//Step 2 - Configuration
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/', routes);
+
+//Step 3 - Check If Application Is In Production
+if(process.env.NODE_ENV === 'production') {
+    //Need to run npm build in client to get build folder.
+    //We need to bring the react application into our server via the build folder.
+    app.use(express.static('client/build'));
+    
+    //Set index.html inside build folder 
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client/build/index.html'))
+    })   
+}
+
+app.listen(PORT, () => {
+    log(`Server is listening at http://localhost: ${PORT}`)
+})
