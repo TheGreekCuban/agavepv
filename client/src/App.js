0c63086371db783f64cd79c1727494912c89cfd7
TheGreekCuban <theodorealexander92@gmaill.com>
2019-12-17 12:20:26 -0500

    nav responsive

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import "./App.css";
 function App() {
   return (
     <Router>
-      <div className="wrapper">
+      <div>
         <Nav />
         <Switch>
           <Route exact path="/" component={Home} />
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
+import Logo from "../../../src/images/logo.png"
 import "./style.css";
 
-function Nav() {
-  return (
-    <nav>
-      <a className="navbar-brand" href="/">
-        Agave Property Ventures
-      </a>
-    </nav>
-  );
+class Nav extends Component {
+  componentDidMount() {
+    const M = window.M
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.sidenav');
+      M.Sidenav.init(elems, {});
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <nav>
+        <div className="nav-wrapper">
+          <a href="/" className="brand-logo"><img className="responsive-img" id="logo" src={Logo} alt="logo"/></a>
+          <a href="#!" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">News</a></li>
+            <li><a href="/criteria">Acquisition Criteria</a></li>
+            <li><a href="/contact">Contact</a></li>
+          </ul>
+        </div>
+      </nav>
+
+      <ul className="sidenav" id="mobile-demo">
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">News</a></li>
+        <li><a href="/criteria">Acquisition Criteria</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul> 
+    </div>
+    );
+  }
 }
 
 export default Nav;
+
