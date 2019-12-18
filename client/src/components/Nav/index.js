f1680470f286b628b3337fa4ed159ff21a4e477c
TheGreekCuban <theodorealexander92@gmaill.com>
2019-12-18 01:59:06 -0500

    added mroe scc

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -17,12 +17,12 @@ class Nav extends Component {
         <nav>
         <div className="nav-wrapper">
           <a href="/" className="brand-logo"><img className="responsive-img" id="logo" src={Logo} alt="logo"/></a>
-          <a href="#!" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+          <a href="#!" data-target="mobile-demo" className="sidenav-trigger"><i id="hamburger" className="material-icons"><strong>menu</strong></i></a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="/about">About</a></li>
-            <li><a href="/contact">News</a></li>
-            <li><a href="/criteria">Acquisition Criteria</a></li>
-            <li><a href="/contact">Contact</a></li>
+            <li><a id="navL" href="/about">About</a></li>
+            <li><a id="navL" href="/contact">News</a></li>
+            <li><a id="navL" href="/criteria">Acquisition Criteria</a></li>
+            <li><a id="navL" href="/contact">Contact</a></li>
           </ul>
         </div>
       </nav>
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import "./style.css";
 
 
 class Home extends Component {
@@ -10,12 +10,11 @@ class Home extends Component {
   render() {
     return (
       <main>
-        <h4>
-          Agave Property Ventures is a real estate development and property management firm located in Newark, NJ.
-          <br />
-          <br />
-          Our mission is to create value for our stakeholders while simultaneously improving the local community.
-        </h4>
+        <div className="description">
+        <h1>Agave Property Ventures</h1>
+          <p>A real estate development and property management firm located in Newark, NJ. Our mission is to create value for our stakeholders while simultaneously improving the local community.
+          </p>
+        </div>
       </main>
     );
   }
