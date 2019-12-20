2a287eeeeff4996d65aa58ccf5b8d1d769a317e1
TheGreekCuban <theodorealexander92@gmaill.com>
2019-12-19 21:33:26 -0500

    moved router to index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Home from "./pages/Home/index.js";
 import NoMatch from "./pages/NoMatch/index.js";
 import Nav from "./components/Nav";
@@ -12,7 +12,6 @@ import "./App.css";
 
 function App() {
   return (
-    <Router>
       <>
         <Nav />
         <Switch>
@@ -25,7 +24,6 @@ function App() {
         </Switch>
         <Footer />
       </>
-    </Router>
   );
 }
 
