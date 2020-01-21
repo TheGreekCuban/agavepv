04d194411bf2aea37fb5467c4fb11681d46471bf
TheGreekCuban <theodorealexander92@gmaill.com>
2020-01-20 21:21:58 -0500

    cert route added

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,14 @@ const contactRoutes = require("./contact");
 // API Routes
 router.use("/contact", contactRoutes);
 
+//Cert Route
+router.get("/.well-known/acme-challenge/uVl6ax2J0FfvbJf5sdgq1DYZ4nDy-2QzqVzAR2dhrh4", (request, response) => { 
+  response.send("uVl6ax2J0FfvbJf5sdgq1DYZ4nDy-2QzqVzAR2dhrh4.afJbyYS_pgF6jTylFrprrYVKIwNMppgpjVIax50nZZM") 
+});
+
 // If no API routes are hit, send the React app
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+router.use(function(request, response) {
+  response.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 module.exports = router;
