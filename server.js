8fee0c0f6d89a2f258bd4b8516ae3e4cb0153f15
TheGreekCuban <theodorealexander92@gmaill.com>
2020-01-22 17:19:03 -0500

    added old code back just incase

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//const sslRedirect = require("heroku-ssl-redirect")
+const sslRedirect = require("heroku-ssl-redirect")
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
@@ -9,23 +9,22 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if(process.env.NODE_ENV === 'production') { 
 // Serve up static assets (usually on heroku)
-if(process.env.NODE_ENV === 'production') {
   app.use(express.static("client/build"))
   app.use((req, res, next) => {
   if (req.header('x-forwarded-proto') !== 'https') {
     res.redirect(`https://${req.header('host')}${req.url}`);
-  } else {
-   next()
-  }
+  } else { next() }
   })
 }
+
 
 // Add routes, both API and view
 app.use(routes);
 
 // Adds the sslRedirect for https://
-//app.use(sslRedirect())
+//app.use(sslRedirect(['production'], 301))
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/agavepv");
