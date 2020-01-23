025a5de05e103d6e6f0efa093ee9af9b5439254a
TheGreekCuban <theodorealexander92@gmaill.com>
2020-01-22 19:48:59 -0500

    try this without build app use

--- server.js
+++ server.js
@@ -11,14 +11,15 @@ app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(sslRedirect(['production', 'development', 'other'], 301))
+  //app.use(express.static("client/build"));
 }
 
 // Add routes, both API and view
 app.use(routes);
 
 // Adds the sslRedirect for https://
-app.use(sslRedirect(['production', 'development', 'other'], 301))
+
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/agavepv");
