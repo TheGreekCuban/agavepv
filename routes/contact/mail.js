6fcdf3d0e7c1cc9bbad3767af887a8446ee4044e
TheGreekCuban <theodorealexander92@gmaill.com>
2020-03-09 17:09:45 -0400

    stage this2

--- routes/contact/mail.js
+++ routes/contact/mail.js
@@ -14,9 +14,3 @@ router.route("/")
 //   .delete(leadController.remove);
 
 module.exports = router;
-const router = require("express").Router();
-const leadController = require("../../controllers/leadController");
-
-// Matches with "/contact/mail"
-router.route("/")
-  .post(leadController.send);
