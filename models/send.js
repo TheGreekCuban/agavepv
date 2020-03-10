ac07185ff3e221427f94a130ba847d62b5724173
TheGreekCuban <theodorealexander92@gmaill.com>
2020-03-10 16:04:53 -0400

    removed route133345

--- models/send.js
+++ models/send.js
@@ -15,7 +15,7 @@ const Send = (request) => {
     `
 
     // call the main function which will take care of the nodemailer.
-    main(output)
+    return main(output)
 }
 
 async function main(output) {
