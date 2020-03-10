27574cac1b93dca2329bd4c78917559da5f3dbf6
TheGreekCuban <theodorealexander92@gmaill.com>
2020-03-10 19:54:34 -0400

    z

--- routes/contact/mail.js
+++ routes/contact/mail.js
@@ -1,17 +1,17 @@
-const router = require("express").Router();
+const express = require('express')
+const app = express()
 const leadController = require("../../controllers/leadController");
 
 // Matches with "/contact/mail"
 // This is a route where we either go to the lead controller and make the message get sent there, or do the work here.
-router.route("/")
-.post((req, res, next) => {
+app.post("/", (req, res, next) => {
   console.log(
     ` req[1]: ${req}
       res[1]: ${res}
       next: ${next}
     `
   )
-  leadController.send()
+  leadController.send(req)
 });
 
 // Matches with "/api/leads/:id"
