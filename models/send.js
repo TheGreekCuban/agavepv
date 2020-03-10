e1602f4e905ae704b1c59104e04c2948f0786d48
TheGreekCuban <theodorealexander92@gmaill.com>
2020-03-10 16:44:45 -0400

    removed route133345674

--- models/send.js
+++ models/send.js
@@ -37,16 +37,14 @@ async function main(output) {
     });
 
  
+   // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 
-    console.log("Message sent: %s", info.messageId);
+}
+    return console.log("Message sent: %s", info.messageId);
     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-    // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-
-    return console.log(`Thank you ${output.Name}, your message has been sent. Someone will be in contact with you shortly!`)
-}
+ 
 
 // Export the model
 module.exports = Send; 
