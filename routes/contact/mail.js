950bce79bef21ead636263746d19cfb7826723c7
TheGreekCuban <theodorealexander92@gmaill.com>
2020-03-10 19:43:38 -0400

    axios

--- routes/contact/mail.js
+++ routes/contact/mail.js
@@ -3,9 +3,15 @@ const leadController = require("../../controllers/leadController");
 
 // Matches with "/contact/mail"
 // This is a route where we either go to the lead controller and make the message get sent there, or do the work here.
-router.post("/", (req, res) => {
-  console.log("req[3]: ", req, "res[3]: ", res)
-  leadController.send
+router.route("/")
+.post((req, res, next) => {
+  console.log{
+    ` req[1]: ${req}
+      res[1]: ${res}
+      next: ${next}
+    `
+  }
+  leadController.send()
 });
 
 // Matches with "/api/leads/:id"
