a6e9ba138a11b926c60aaa27fdaf8d16108379fa
TheGreekCuban <theodorealexander92@gmaill.com>
2020-03-12 14:12:45 -0400

    ssle red

--- server.js
+++ server.js
@@ -16,11 +16,6 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Add routes, both API and view
-app.use(routes);
-
-// Adds the sslRedirect for https://
-app.use(sslRedirect(['production'], 301))
 
 // Connect to the Mongo DB
 mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
@@ -28,6 +23,12 @@ mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
 db.on("error", console.error.bind(console, "connection error: "))
 db.once("open", () => console.log(`Connected to mongoose!`))
 
+
+// Add routes, both API and view
+app.use(routes);
+
+// Adds the sslRedirect for https://
+app.use(sslRedirect(['production'], 301))
 
 app.listen(PORT, function(){
     console.log(`Server is listening at http://localhost:${PORT}/`)
