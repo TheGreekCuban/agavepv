5c85bfb7c322f959576a7690bdf5e154dcd9fc4b
TheGreekCuban <theodorealexander92@gmaill.com>
2020-03-12 14:22:54 -0400

    revert

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/agavepv"
-const db = mongoose.connection
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -16,20 +13,16 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-
-// Connect to the Mongo DB
-mongoose.connect(MONGODB_URI);
-
-db.on("error", console.error.bind(console, "connection error: "))
-db.once("open", () => console.log(`Connected to mongoose!`))
-
-
 // Add routes, both API and view
 app.use(routes);
 
 // Adds the sslRedirect for https://
 app.use(sslRedirect(['production'], 301))
 
-app.listen(PORT, function(){
-    console.log(`Server is listening at http://localhost:${PORT}/`)
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/agavepv");
+
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is listening at http://localhost:${process.env.PORT}`)
 })
