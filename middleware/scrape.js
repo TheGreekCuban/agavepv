103e55e6315885fd2a361260bd76149606e432ca
TheGreekCuban <theodorealexander92@gmaill.com>
2020-04-10 20:52:43 -0400

    backend connected and promises setup

--- middleware/scrape.js
+++ middleware/scrape.js
@@ -2,40 +2,35 @@ const db = require("../models")
 const axios = require("axios")
 const cheerio = require("cheerio")
 
-const Scrape = async(req, res) => {
-    return "this is the return"
-    /*
+const Scrape = (req, res) => {
+
     //First we grab the body of the html with axios
-    axios.get("https://www.tapinto.net/towns/newark/sections/development").then(response => {
+    return axios.get("https://www.tapinto.net/towns/newark/sections/development").then(response => {
         console.log("made it to back end")
         //Then we load that into cheerio and save it to $ for the shorthand selector
         let $ = cheerio.load(response.data)
-        console.log(`Scraper response: ${response.data}`)
-        let grabbedElement = $(".to-flex title-with-sponsor").find("a")
+        let grabbedElement = $("ul .list_article").find("li .select")
 
         //Now, we grab every headline title tag and do the following
         $(grabbedElement).each((i, element) => {
-
+           
             //Save an empty result object
             const result = {}
 
             //Add the text and href of every link, save them as properties of result
             result.title = $(element).find(".container-section-name").text()
-
+ 
             //Create a new Scraper using the result obj built from scraping. First we search for a match by using findOne, if there is no match in the database then we create a new collection.
             db.Scraper
-            .findOne({ title: "test" }, (error, existingArticle) => {
+            .findOne({ title: result.titile }, (error, existingArticle) => {
                 if (existingArticle === null) {
-                    console.log(existingArticle)
-                    db.Scraper
-                        .create({tile: "test"})
+                    db.Scraper.create(result)
                         .then(dbScraper => console.log(dbScraper))
                         .catch(error => console.log(error))
                 }
             })
         })       
     })
-    */
 }
 
 module.exports = Scrape
