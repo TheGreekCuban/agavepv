c653c151091666c1746cf2cfc41e8d6bb517258b
TheGreekCuban <theodorealexander92@gmaill.com>
2020-04-14 18:54:18 -0400

    moved scrape to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer"
 import About from "./pages/About/index.js"
 import News from "./pages/News/index.js"
 import Contact from "./pages/Contact/index.js"
+import API from "./utils/API"
 import "./App.css";
 import Portfolio from "./pages/Portfolio/index.js";
 
-function App() {
-  return (
-    <>
-      <Nav />
-      <About />
-      <Portfolio />
-      <Contact />
-      <News />
-      <Footer />
+class App extends Component {
+
+  state = {
+    news: []
+  };
+
+  scrapeArticles = () => {
+    API.scrapeArticles()
+      .then(news => this.setState({news: news.data}))
+      .catch(error => console.log(error))
+  }
+
+  componentDidMount() {
+    this.scrapeArticles()
+  }
+
+  render() {
+    return (
+      <>
+        <Nav />
+        <About />
+        <Portfolio />
+        <Contact />
+        <News news={this.state.news} />
+        <Footer />
     </>
-  );
+    );
+  }
+
 }
 
 export default App;
--- client/src/pages/News/index.js
+++ client/src/pages/News/index.js
@@ -1,32 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import "./style.css";
-import API from "../../utils/API"
 import  { ArticleList } from "../../components/ArticleList"
 
 
-class News extends Component {
-  state = {
-    news: []
-  };
+function News(props) {
 
-  scrapeArticles = () => {
-    API.scrapeArticles()
-      .then(news => this.setState({news: news.data}))
-      .catch(error => console.log(error))
-  }
-
-  componentDidMount() {
-    this.scrapeArticles()
-  }
-
-  render() {
     return (
       <main id="news" className="section scrollspy">
         <h1>More About Newark, NJ</h1>
-        <ArticleList news={this.state.news}></ArticleList>
+        <ArticleList news={props.news}></ArticleList>
       </main>
     );
-  }
 }
 
 export default News;
